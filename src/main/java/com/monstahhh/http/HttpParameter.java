71c2527dce84fbc63d3c09d53ffcc0741f63deee
ThybeVB <vanbeerselthybe@gmail.com>
2021-12-05 22:39:10 +0100

    Refurbish Translate command, but needs reworking for ease of use

---
+++ src/main/java/com/monstahhh/http/HttpParameter.java
@@ -0,0 +1,21 @@
+package com.monstahhh.http;
+
+public class HttpParameter {
+
+    private final String name;
+    private final String value;
+
+    public HttpParameter(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+}
--- src/main/java/com/monstahhh/mrworldwide/commands/TranslateCommand.java
+++ src/main/java/com/monstahhh/mrworldwide/commands/TranslateCommand.java
@@ -1,8 +1,23 @@
 package com.monstahhh.mrworldwide.commands;
 
+import com.monstahhh.http.HttpClient;
+import com.monstahhh.http.HttpMethod;
+import com.monstahhh.http.HttpResponse;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TranslateCommand extends ListenerAdapter {
 
@@ -14,5 +29,74 @@ public class TranslateCommand extends ListenerAdapter {
         String sentenceToBeTranslated = event.getOption("sentence").getAsString();
         String language = event.getOption("originallanguage").getAsString();
         String destinationLanguage = event.getOption("destinationlanguage").getAsString();
+
+        String result = getTranslation(language, destinationLanguage, sentenceToBeTranslated);
+        EmbedBuilder eb = new EmbedBuilder();
+
+        if (result == null) {
+            event.reply("There was an error translating the sentence. Please try again later.").setEphemeral(false).queue();
+        } else {
+            String link = String.format("https://translate.google.com?sl=%s&tl=%s&op=translate&text=", language, destinationLanguage);
+            String inputText = URLEncoder.encode(sentenceToBeTranslated, StandardCharsets.UTF_8);
+            eb.setTitle("Google Translate", link + inputText);
+
+            String originLocale = new Locale(language).getDisplayLanguage(Locale.ENGLISH);
+            String wantedLocale = new Locale(destinationLanguage).getDisplayLanguage(Locale.ENGLISH);
+
+            eb.addField(originLocale + " -> " + wantedLocale, sentenceToBeTranslated + " -> " + result, false);
+            eb.setColor(Color.BLUE);
+
+            event.replyEmbeds(eb.build()).setEphemeral(false).queue();
+        }
+    }
+
+    private String getTranslation(String origin, String destination, String msg) {
+        String result = null;
+
+        try {
+            result = requestTranslation(msg, origin, destination);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return result;
+    }
+
+    private String requestTranslation(String textToTranslate, String from, String to) throws IOException {
+
+        if (textToTranslate.contains(". ")) {
+            String[] sentences = textToTranslate.split("\\.");
+
+            StringBuilder sb = new StringBuilder();
+            for (String sentence : sentences) {
+                String singleResult = castResult(doTranslateFor(sentence, from, to).asString());
+                sb.append(singleResult);
+                sb.append(". ");
+            }
+
+            return sb.toString();
+        } else {
+            return castResult(doTranslateFor(textToTranslate, from, to).asString());
+        }
+    }
+
+    private HttpResponse doTranslateFor(String textToTranslate, String from, String to) throws IOException {
+        String encodedText = java.net.URLEncoder.encode(textToTranslate, StandardCharsets.UTF_8);
+        String paramsLink = "single?client=gtx&sl=%s&tl=%s&ie=UTF-8&oe=UTF-8&dt=t&dt=rm&q=%s";
+        String params = String.format(paramsLink, from, to, encodedText);
+
+        return new HttpClient().request(HttpMethod.GET, ("https://translate.googleapis.com/translate_a/" + params));
+    }
+
+    private String castResult(String result) {
+        Pattern pat = Pattern.compile("\"(.*?)\"");
+        List<String> allMatches = new ArrayList<>();
+
+        Matcher matcher = pat.matcher(result);
+        while (matcher.find()) {
+            allMatches.add(matcher.group().replace("\"", ""));
+        }
+
+        return allMatches.get(0);
     }
 }
