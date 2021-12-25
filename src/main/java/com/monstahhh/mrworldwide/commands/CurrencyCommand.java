13212f68269ac7fec0fe526a84f429ce5cc4b3db
ThybeVB <vanbeerselthybe@gmail.com>
2021-12-25 19:31:06 +0100

    Begin Currency command

---
+++ src/main/java/com/monstahhh/mrworldwide/commands/CurrencyCommand.java
@@ -0,0 +1,13 @@
+package com.monstahhh.mrworldwide.commands;
+
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class CurrencyCommand extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommand(@NotNull SlashCommandEvent event) {
+        //TODO
+    }
+}
